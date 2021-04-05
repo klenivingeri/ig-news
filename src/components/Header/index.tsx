e51f0f5a44917c89be2acc31989f89ce7d3a1cd1
Erick <libertti@live.com>
2021-04-05 12:59:13 -0300

    Header Configurado para todas as paginas

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import styles from './styles.module.scss'
+
+export function Header(){
+    return(
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="ig.news"/>
+                <nav>
+                    <a className={styles.active}>Home</a>
+                    <a>Posts</a>
+
+                </nav>
+                
+            </div>
+
+        </header>
+    );
+}
+
+
+/**
+ * 
+ *  Utilizando Next não precisa fazer a importação
+ *  mas se quiser pode instalar o pacote o nextimages
+ * 
+ */
