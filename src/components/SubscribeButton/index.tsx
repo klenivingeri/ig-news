477078fbdd76d1dde4c4dbe51aed47c4f20a7633
Erick <libertti@live.com>
2021-04-09 11:15:30 -0300

    Redirecionando para o Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,7 @@
 import { useSession, signIn } from 'next-auth/client'
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -7,13 +10,25 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({priceId}:SubscribeButtonProps) {
     const [ session ]  = useSession()
-    function hundleSubscribe(){
+    async function hundleSubscribe(){
 
         if(!session) {
             signIn('github');
             return;
         }
         //Criação da chekout session
+        try {
+            const response = await api.post('/subscribe')
+            
+            const { sessionId } = response.data;
+            
+            const stripe = await getStripeJs();
+
+            await stripe.redirectToCheckout({sessionId})
+        
+        } catch (err){
+            alert(err.message)
+        }
 
     }
     
