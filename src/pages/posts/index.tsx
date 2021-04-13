911325533e8378d7c1f11c59f387f11b299ec6ca
Erick <libertti@live.com>
2021-04-13 12:03:14 -0300

    Listando posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,8 +3,22 @@ import Head from 'next/head';
 import { getPrismicClient } from '../../services/prismic';
 import Prismic from '@prismicio/client'
 import styles from './styles.module.scss';
+import { RichText } from 'prismic-dom';
 
-export default function Posts() {
+
+type Post ={ 
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string
+}
+
+interface PostsProps {
+    posts: Post[]
+}
+
+
+export default function Posts( { posts }: PostsProps) {
     return (
         <>
         <Head>
@@ -12,21 +26,15 @@ export default function Posts() {
         </Head>
         <main className={styles.container}>
             <div className={styles.posts}>
-                <a href="#">
-                    <time>12 março de 2021</time>
-                    <strong> Creating a Monorepo with lorna & yarn workspaces</strong>
-                    <p>Linguagens de baixo nível, como C, tem primitivas de gerenciamento de memória de baixo nível como malloc() e free()</p>
-                </a>
-                <a href="#">
-                    <time>12 março de 2021</time>
-                    <strong> Creating a Monorepo with lorna & yarn workspaces</strong>
-                    <p>Linguagens de baixo nível, como C, tem primitivas de gerenciamento de memória de baixo nível como malloc() e free()</p>
-                </a>
-                <a href="#">
-                    <time>12 março de 2021</time>
-                    <strong> Creating a Monorepo with lorna & yarn workspaces</strong>
-                    <p>Linguagens de baixo nível, como C, tem primitivas de gerenciamento de memória de baixo nível como malloc() e free()</p>
-                </a>
+                {posts.map( post =>(
+
+                    <a key={post.slug} href="#">
+                    <time>{post.updatedAt}</time>
+                    <strong>{post.title}</strong>
+                    <p>{post.excerpt}</p>
+                    </a>
+
+                ))}
             </div>
         </main>
 
@@ -41,12 +49,34 @@ export const getStaticProps: GetStaticProps = async() =>{
     const response = await prismic.query([
         Prismic.predicates.at('document.type','publication')
     ],{
-        fetch: ['publication.title', 'publication,content'],
+        fetch: ['publication.title', 'publication.content'],
         pageSize: 100,
     })
 
-    console.log(JSON.stringify(response,null,2))
+
+    const posts = response.results.map(post => {
+        return {
+            slug: post.id,
+            title: RichText.asText(post.data.title),
+            excerpt:post.data.content.find( content => content.type == 'paragraph')?.text ?? '', //1
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR',{
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+
+            })
+        }
+    })
+
+
+    //console.log(JSON.stringify(response,null,2))
     return{
-        props:{}
+        props:{posts}
     }
 }
+
+
+
+/**
+ * 1 ) caso não localize o primeiro paragrafo, eu não quero o text, quero que o retorno seja vazio
+ */
